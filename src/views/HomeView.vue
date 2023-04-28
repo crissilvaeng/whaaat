99816c749c6a081e5a37f221cbfc36c5000d41f8
Cristina Silva <crissilva.eng@gmail.com>
2023-04-28 00:53:29 -0300

    Never gonna run around and desert you

---
+++ src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { computed, ref } from 'vue'
+import { useRoute } from 'vue-router'
+import nlp from 'compromise'
+
+const route = useRoute()
+const query = computed(() => route.query.q)
+
+const out = ref('')
+
+const doc = nlp(query.value)
+out.value = doc.html({
+  'mark': '#Verb',
+})
+</script>
+
+<template>
+  <p v-if="query" v-html="out" />
+  <p v-else>(┛ಠДಠ)┛彡┻━┻</p>
+</template>
+
+<style>
+p {
+  max-width: 30ch;
+}
+
+mark {
+  -webkit-animation: 1.5s highlight 1.5s 1 normal forwards;
+  animation: 1.5s highlight 1.5s 1 normal forwards;
+  background-color: none;
+  background: linear-gradient(90deg, #7efff5 50%, rgba(255, 255, 255, 0) 50%);
+  background-size: 200% 100%;
+  background-position: 100% 0;
+}
+
+@-webkit-keyframes highlight {
+  to {
+    background-position: 0 0;
+  }
+}
+
+@keyframes highlight {
+  to {
+    background-position: 0 0;
+  }
+}
+</style>
